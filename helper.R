7e39fa902963d346ba4cda9a9e2dfa416a6a1d46
Anıl Alper <68917545+anilalperr@users.noreply.github.com>
2022-10-08 12:53:39 -0500

    Add files via upload

--- helper.R
+++ helper.R
@@ -1,25 +1,24 @@
 # values to store the location information
-address = c()
-latitude = c()
-longitude = c()
-descriptions = c()
-category = c()
-
 event_df <- data.frame(address = c(),
                        latitude = c(),
                        longitude = c(),
                        descriptions = c(),
                        category = c())
 
-validateNumeric <- function(input, t) {
+validateLat <- function(input) {
   numeric_input = as.numeric(input)
-  if (t == "lat") {
-    return (is.numeric(numeric_input) && (-90 <= numeric_input) && (numeric_input <= 90))
-  } else {
-    return (is.numeric(numeric_input) && (-180 <= numeric_input) && (numeric_input <= 180))
-  }
+  return (is.numeric(numeric_input) && (-90 <= numeric_input) && (numeric_input <= 90))
+}
+
+validateLon <- function(input) {
+  numeric_input = as.numeric(input)
+  return (is.numeric(numeric_input) && (-180 <= numeric_input) && (numeric_input <= 180))
 }
 
 validateAddress <- function(input) {
-  return(input)
+  return(input != "")
+}
+
+validateDesc <- function(input) {
+  return(input != "")
 }
